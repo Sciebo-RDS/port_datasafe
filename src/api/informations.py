c48da6dce7ac0d37f6bafd0999bdb866d9a346dc
Peter Heiss <peter.heiss@uni-muenster.de>
2020-12-01 11:46:30 +0100

    first changes for informations

--- src/api/informations.py
+++ src/api/informations.py
@@ -4,8 +4,8 @@ from RDS import FileTransferMode, LoginMode
 def index():
     data = {
         "fileTransferArchive":"zip",
-        "fileTransferMode":FileTransferMode.passive,
-        "loginMode":LoginMode.credentials,
+        "fileTransferMode":FileTransferMode.passive.value,
+        "loginMode":LoginMode.credentials.value,
         "credentials": {
             "userId":False,
             "password":False
