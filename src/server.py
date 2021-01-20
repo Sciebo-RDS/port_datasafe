13d0842d261d3dd393fc4ce09e2e9664ca780b18
Peter Heiss <peter.heiss@uni-muenster.de>
2021-01-20 16:47:27 +0100

    use RDS specific Util registering func

--- src/server.py
+++ src/server.py
@@ -3,11 +3,17 @@
 from __init__ import app, register_service
 import os
 
-register_service(
-    "Datasafe"
+from RDS import Util, LoginService, FileTransferMode, FileTransferArchive
+service = LoginService(
+    servicename="Datasafe",
+    implements=["metadata"],
+    fileTransferMode=FileTransferMode.passive,
+    fileTransferArchive=FileTransferArchive.none,
+    userId=False,
+    password=False
 )
+Util.register_service(service)
 
 # set the WSGI application callable to allow using uWSGI:
 # uwsgi --http :8080 -w app
 app.run(port=8080, server="gevent")
-
